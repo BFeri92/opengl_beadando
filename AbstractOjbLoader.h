6fc43c826ed24b00b5a9649526236bccc76bf3bf
Ferenc Barta <barta.ferenc01@gmail.com>
2014-07-07 18:39:41 +0200

    AbstractObjLoader csak egy metódust tartalmaz.
    Nem fogjuk többször elkérni a triangleBatch-t, így nem hasznos
    különválasztani a beolvasást és a batch elkérését.

--- AbstractOjbLoader.h
+++ AbstractOjbLoader.h
@@ -3,10 +3,7 @@
 class AbstractObjLoader
 {
 	public:
-		//A paraméterben megadott .obj fájlból be kell, hogy olvassa
-		//a mesht. A face-k vertex/texture-coordinate formátumban lesznek megadva.
-		virtual void loadObject(std::string fname) = 0;
-		//A beolvasott mesh alapján készített GLTrianlgeBatch-t kell
-		//visszaadnia.
-		virtual const GLTriangleBatch& getBatch() = 0;
+		//A paraméterként adott fájlnévhez tartozó fájlból kell beolvasnia
+		//a mesht, felépíteni belőle egy GLTriangleBatch-t és azt visszaadni.
+		virtual const GLTriangleBatch getBatch(std::string fname) = 0;
 };
