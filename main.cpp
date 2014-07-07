7faa630c167dbcd2a0325bfca25b860983ad6f30
Ferenc Barta <barta.ferenc01@gmail.com>
2014-07-07 22:27:12 +0200

    GLUT callback-ek egy része
    Billentyű lenyomásánál segfault, mivel a Game még nem inicializálja
    az eventHandler-t.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,56 @@
-#include <AbstractOjbLoader.h>
-int main()
+#include "AbstractOjbLoader.h"
+#include "Game.h"
+#include <GLTools.h>
+#ifdef __APPLE__
+#include <glut/glut.h>
+#else
+#define FREEGLUT_STATIC
+#include <GL/glut.h>
+#endif
+
+
+void SetupRC(void)
 {
-	
+}
+
+
+void RenderScene(void)
+{
+	Game::getInstance().render();
+}
+
+void ShutdownRC(void)
+{
+}
+
+void ChangeSize(int w, int h)
+{
+}
+
+void KeyPressFunc(unsigned char key, int x, int y)
+{
+	Game::getInstance().getEventHandler().keyDownEvent(key);
+}
+
+int main(int argc, char* argv[])
+{
+    gltSetWorkingDirectory(argv[0]);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("");
+    glutReshapeFunc(ChangeSize);
+    glutDisplayFunc(RenderScene);
+    glutKeyboardFunc(KeyPressFunc);
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+        fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
+        return 1;
+    }
+
+    SetupRC();
+    glutMainLoop();
+    ShutdownRC();
+    return 0;
 }
