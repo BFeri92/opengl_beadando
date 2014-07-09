95f48ec27cd20a32ea11eaa5f1a76bb6037deaa6
Ferenc Barta <barta.ferenc01@gmail.com>
2014-07-09 19:37:57 +0200

    Absztrakt EventHandler metódusok helyett üres törzsű metódusok.
    Így nem kell kifejteni azokat a metódusokat, amikre
    eselteg nincs szükség.

---
+++ EventHandler.cpp
@@ -0,0 +1,23 @@
+#include "EventHandler.h"
+void EventHandler::keyDownEvent(char key) 
+{
+	
+}
+
+void EventHandler::keyUpEvent(char key) 
+{
+	
+}
+
+void EventHandler::activeMouseMotionEvent(int x, int y) 
+{
+	
+}
+void EventHandler::mouseUpEvent(int button, int x, int y) 
+{
+	
+}
+void EventHandler::mouseDownEvent(int button, int x, int y) 
+{
+	
+}
--- EventHandler.h
+++ EventHandler.h
@@ -2,10 +2,10 @@
 #define EVENTHANDLER_H_INCLUDED
 class EventHandler{
 	public:
-		virtual void keyDownEvent(char key) = 0;
-		virtual void keyUpEvent(char key) = 0;
-		virtual void activeMouseMotionEvent(int x, int y) = 0;
-		virtual void mouseUpEvent(int button, int x, int y) = 0;
-		virtual void mouseDownEvent(int button, int x, int y) = 0;
+		virtual void keyDownEvent(char key);
+		virtual void keyUpEvent(char key);
+		virtual void activeMouseMotionEvent(int x, int y);
+		virtual void mouseUpEvent(int button, int x, int y);
+		virtual void mouseDownEvent(int button, int x, int y);
 };
 #endif
