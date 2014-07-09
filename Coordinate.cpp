d499d5b94e97f4f4fbce782ae9c9ea24911232ee
Gyulai Zsolt <gyulai_zsolt@hotmail.com>
2014-07-09 16:39:55 +0200

    Coordinate osztály

---
+++ Coordinate.cpp
@@ -0,0 +1,8 @@
+#include "Coordinate.h"
+
+Coordinate::Coordinate(){};
+Coordinate::Coordinate(GLint _x ,GLint _y){
+    x=_x;
+    y=_y;
+}
+
---
+++ Coordinate.h
@@ -0,0 +1,11 @@
+#ifndef COORDINATE_H_INCLUDED
+#define COORDINATE_H_INCLUDED
+struct Coordinate{
+GLint x;
+GLint y;
+
+Coordinate();
+Coordinate(GLint _x ,GLint _y);
+
+}
+#endif
